977787572a99eeb61cdb496651bc275131b6c7e4
ahmednassereglan <ahmednassereglan@gmail.com>
2021-08-29 15:49:02 +0200

    install auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes();
+// Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/{page}', 'AdminController@index');
